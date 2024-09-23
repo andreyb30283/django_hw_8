57ec389f776fa00d9700dafad514504a1af162f0
Andrii_Butov <kot09542@gmail.com>
2024-09-23 22:52:47 +0200

    HW_9 (meta,str,new obj) category_display

--- myapp/admin.py
+++ myapp/admin.py
@@ -3,12 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from .models import Task, Category, SubTask
 
+
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'deadline', 'creates_at')
+
 
 class SubTaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'deadline', 'created_at', 'task')
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
 admin.site.register(Task, TaskAdmin)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(SubTask, SubTaskAdmin)
