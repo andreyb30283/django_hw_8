279d4ce3f74aeb86cfd41fe69999b7408f21d958
Andrii_Butov <kot09542@gmail.com>
2024-10-23 20:53:34 +0200

    # Web development: домашние задание 13 (Python) # Домашнее задание: Замена функций представлений на Generic Views для # задач и подзадач # Цель: # Изучить Generic Views. # Используя Generic Views, замените существующие классы представлений # для задач (Tasks) и подзадач (SubTasks) на соответствующие классы для # полного CRUD (Create, Read, Update, Delete) функционала. Агрегирующий # эндпойнт для статистики задач оставьте как есть. Реализуйте пагинацию, # фильтрацию, поиск и сортировку для обоих наборов представлений. # Задание 1: Замена представлений для задач (Tasks) на Generic Views # Шаги для выполнения: # Замените классы представлений для задач на Generic Views: # Используйте ListCreateAPIView для создания и получения списка задач. # Используйте RetrieveUpdateDestroyAPIView для получения, обновления и # удаления задач. # Реализуйте пагинацию, фильтрацию, поиск и сортировку: # Добавьте пагинацию для списка задач. # Реализуйте фильтрацию по полям status и deadline. # Реализуйте поиск по полям title и description. # Добавьте сортировку по полю created_at. # Задание 2: Замена представлений для подзадач (SubTasks) на Generic Views # Шаги для выполнения: # Замените классы представлений для подзадач на Generic Views: # Используйте ListCreateAPIView для создания и получения списка подзадач. # Используйте RetrieveUpdateDestroyAPIView для получения, обновления и удаления подзадач. # Реализуйте пагинацию, фильтрацию, поиск и сортировку: # Добавьте пагинацию для списка подзадач. # Реализуйте фильтрацию по полям status и deadline. # Реализуйте поиск по полям title и description. # Добавьте сортировку по полю created_at. # Оформление ответа: # Предоставьте решение: Прикрепите ссылку на гит. # Скриншоты тестирования: Приложите скриншоты из браузера или Postman, # подтверждающие успешное создание, обновление, получение и удаление # данных через API.

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,64 @@
-from .views import TaskCreateView, TaskListView, TaskStatsView, SubTaskDetailUpdateDeleteView, SubTaskListCreateView
-from django.urls import path
+# from .views import TaskCreateView, TaskListView, TaskStatsView, SubTaskDetailUpdateDeleteView, SubTaskListCreateView
+# from django.urls import path
+#
+#
+# urlpatterns = [
+#     path('tasks/create/', TaskCreateView.as_view(), name='task-create'),
+#     path('tasks/stats/', TaskStatsView.as_view(), name='task-stats'),
+#     path('tasks/', TaskListView.as_view(), name='task-list'),
+#
+#
+#     path('subtasks/', SubTaskListCreateView.as_view(), name='subtask-list-create'),
+#     path('subtasks/<int:pk>/', SubTaskDetailUpdateDeleteView.as_view(), name='subtask-detail-update-delete'),
+# ]
+#
+#
+# ________________________________________________________________________________
 
+# Web development: домашние задание 13 (Python)
+# Домашнее задание: Замена функций представлений на Generic Views для
+# задач и подзадач
+# Цель:
+# Изучить Generic Views.
+# Используя Generic Views, замените существующие классы представлений
+# для задач (Tasks) и подзадач (SubTasks) на соответствующие классы для
+# полного CRUD (Create, Read, Update, Delete) функционала. Агрегирующий
+# эндпойнт для статистики задач оставьте как есть. Реализуйте пагинацию,
+# фильтрацию, поиск и сортировку для обоих наборов представлений.
+# Задание 1: Замена представлений для задач (Tasks) на Generic Views
+# Шаги для выполнения:
+# Замените классы представлений для задач на Generic Views:
+# Используйте ListCreateAPIView для создания и получения списка задач.
+# Используйте RetrieveUpdateDestroyAPIView для получения, обновления и
+# удаления задач.
+# Реализуйте пагинацию, фильтрацию, поиск и сортировку:
+# Добавьте пагинацию для списка задач.
+# Реализуйте фильтрацию по полям status и deadline.
+# Реализуйте поиск по полям title и description.
+# Добавьте сортировку по полю created_at.
+# Задание 2: Замена представлений для подзадач (SubTasks) на Generic Views
+# Шаги для выполнения:
+# Замените классы представлений для подзадач на Generic Views:
+# Используйте ListCreateAPIView для создания и получения списка подзадач.
+# Используйте RetrieveUpdateDestroyAPIView для получения, обновления и удаления подзадач.
+# Реализуйте пагинацию, фильтрацию, поиск и сортировку:
+# Добавьте пагинацию для списка подзадач.
+# Реализуйте фильтрацию по полям status и deadline.
+# Реализуйте поиск по полям title и description.
+# Добавьте сортировку по полю created_at.
+# Оформление ответа:
+# Предоставьте решение: Прикрепите ссылку на гит.
+# Скриншоты тестирования: Приложите скриншоты из браузера или Postman,
+# подтверждающие успешное создание, обновление, получение и удаление
+# данных через API.
+
+from django.urls import path
+from .views import TaskListCreateView, TaskRetrieveUpdateDestroyView, SubTaskListCreateView, \
+    SubTaskRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('tasks/create/', TaskCreateView.as_view(), name='task-create'),
-    path('tasks/stats/', TaskStatsView.as_view(), name='task-stats'),
-    path('tasks/', TaskListView.as_view(), name='task-list'),
-
-
+    path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
+    path('tasks/<int:pk>/', TaskRetrieveUpdateDestroyView.as_view(), name='task-detail'),
     path('subtasks/', SubTaskListCreateView.as_view(), name='subtask-list-create'),
-    path('subtasks/<int:pk>/', SubTaskDetailUpdateDeleteView.as_view(), name='subtask-detail-update-delete'),
+    path('subtasks/<int:pk>/', SubTaskRetrieveUpdateDestroyView.as_view(), name='subtask-detail'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,64 +1,64 @@
-from django.shortcuts import render
-from django.utils import timezone
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.renderers import JSONRenderer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.db.models import Count, Q
-from rest_framework import generics, filters, status
-from django_filters.rest_framework import DjangoFilterBackend
-from .models import Task
-from .serializers import TaskSerializer, SubTaskCreateSerializer, TaskCreateSerializer
-
-
-class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 4  # Количество объектов на странице
-    page_size_query_param = 'page_size'  # Позволяет пользователю передавать количество элементов на странице
-    max_page_size = 6  # Максимальное количество объектов на одной странице
-
-
-class TaskCreateView(generics.CreateAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskCreateSerializer
-
-    def create(self, request, *args, **kwargs):
-        if isinstance(request.data, list):
-            serializer = self.get_serializer(data=request.data, many=True)
-        else:
-            serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-    # def perform_create(self, serializer):
-    #     # Добавляем автора задачи перед сохранением  serializer.save(author=self.request.user)
-    #     serializer.save()
-
-
-class TaskStatsView(generics.RetrieveAPIView):
-    def get(self, request, *args, **kwargs):
-        total_tasks = Task.objects.count()
-        status_counts = Task.objects.values('status').annotate(count=Count('status'))
-        overdue_tasks = Task.objects.filter(deadline__lt=timezone.now(),
-                                            status__in=['New', 'In progress', 'Pending']).count()
-
-        return Response({
-            'total_tasks': total_tasks,
-            'status_counts': status_counts,
-            'overdue_tasks': overdue_tasks
-        })
-
-
-class TaskListView(generics.ListAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    filterset_fields = ['status', 'deadline']
-    ordering_fields = ['deadline']
-    pagination_class = CustomPageNumberPagination
-
-    # renderer_classes = [JSONRenderer]  # Отключает рендеринг HTML форм
+# from django.shortcuts import render
+# from django.utils import timezone
+# from rest_framework.pagination import PageNumberPagination
+# from rest_framework.renderers import JSONRenderer
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from django.db.models import Count, Q
+# from rest_framework import generics, filters, status
+# from django_filters.rest_framework import DjangoFilterBackend
+# from .models import Task
+# from .serializers import TaskSerializer, SubTaskCreateSerializer, TaskCreateSerializer
+#
+#
+# class CustomPageNumberPagination(PageNumberPagination):
+#     page_size = 4  # Количество объектов на странице
+#     page_size_query_param = 'page_size'  # Позволяет пользователю передавать количество элементов на странице
+#     max_page_size = 6  # Максимальное количество объектов на одной странице
+#
+# # '''
+# class TaskCreateView(generics.CreateAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskCreateSerializer
+#
+#     def create(self, request, *args, **kwargs):
+#         if isinstance(request.data, list):
+#             serializer = self.get_serializer(data=request.data, many=True)
+#         else:
+#             serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         self.perform_create(serializer)
+#         headers = self.get_success_headers(serializer.data)
+#         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+#
+#     # def perform_create(self, serializer):
+#     #     # Добавляем автора задачи перед сохранением  serializer.save(author=self.request.user)
+#     #     serializer.save()
+#
+#
+# class TaskStatsView(generics.RetrieveAPIView):
+#     def get(self, request, *args, **kwargs):
+#         total_tasks = Task.objects.count()
+#         status_counts = Task.objects.values('status').annotate(count=Count('status'))
+#         overdue_tasks = Task.objects.filter(deadline__lt=timezone.now(),
+#                                             status__in=['New', 'In progress', 'Pending']).count()
+#
+#         return Response({
+#             'total_tasks': total_tasks,
+#             'status_counts': status_counts,
+#             'overdue_tasks': overdue_tasks
+#         })
+#
+#
+# class TaskListView(generics.ListAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+#     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+#     filterset_fields = ['status', 'deadline']
+#     ordering_fields = ['deadline']
+#     pagination_class = CustomPageNumberPagination
+# '''
+# renderer_classes = [JSONRenderer]  # Отключает рендеринг HTML форм
 
 
 #
@@ -134,7 +134,7 @@ class TaskListView(APIView):
 '''
 
 '''_________________________________________________________________________________________________'''
-
+'''
 # Список и создание подзадач
 
 from rest_framework.views import APIView
@@ -179,4 +179,41 @@ class SubTaskDetailUpdateDeleteView(APIView):
     def delete(self, request, pk, *args, **kwargs):
         subtask = get_object_or_404(SubTask, pk=pk)
         subtask.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)'''
+
+'''__________________________________________________________________________'''
+
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from myapp.models import *
+from .serializers import *
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class TaskListCreateView(ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ["status", "deadline"]
+    search_fields = ['title', "description"]
+    ordering_fields = ['created_at']
+    ordering = ['created_at']
+
+
+class TaskRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class SubTaskListCreateView(ListCreateAPIView):
+    queryset = SubTask.objects.all()
+    serializer_class = SubTaskSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['status', 'deadline']
+    search_fields = ['title', 'description']
+    ordering = ['created_at']
+
+
+class SubTaskRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = SubTask.objects.all()
+    serializer_class = SubTaskSerializer
